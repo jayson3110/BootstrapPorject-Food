21ee54e22b88e412fcc7889465c9b256651f371a
jayson3110 <jayson7498@gmail.com>
2021-05-10 18:19:13 +0700

    done the projectFood but need to fix Pag

---
+++ main.js
@@ -0,0 +1,13 @@
+function openNav(argument) {
+	// body...
+
+	document.getElementById('mySidenar').style.width="550px";
+    document.getElementById('mySidenar').style.boxShadow ="1px 2px 5px 2px #ccc";
+
+}
+
+function closeNav() {
+	document.getElementById('mySidenar').style.width ="0";
+	document.getElementById('mySidenar').style.boxShadow ="none";
+
+}
